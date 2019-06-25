96da09c96486335766c9d3c522c929e931f5797c
Andrey Surovov <andrey.surovov@gmail.com>
2019-06-25 18:10:56 +0300

    [#5] write docs for decorators

---
+++ docs/decorators.rst
@@ -0,0 +1,13 @@
+.. _decorators:
+
+The django-admin-tools decorators API
+==============================================
+
+This section describe the API of the django-admin-tools decorators.
+
+
+The field decorators
+------------------------
+
+.. automodule:: admin_tools.decorators
+    :members:
--- src/admin_tools/decorators.py
+++ src/admin_tools/decorators.py
@@ -12,18 +12,33 @@ def getattr_nested(obj, attr):
 def admin_link(attr, short_description=None, empty_description="-"):
     """Decorator used for rendering a link to a related model in
     the admin detail page.
-    attr (str):
+
+    ``attr (str)``
         Name of the related field.
-    short_description (str):
+    ``short_description (str)``
         Name if the field.
-    empty_description (str):
+        Default value: None.
+    ``empty_description (str)``
         Value to display if the related field is None.
+        Default value: -.
+
     The wrapped method receives the related object and should
     return the link text.
-    Usage:
-        @admin_link('credit_card', _('Credit Card'))
-        def credit_card_link(self, credit_card):
-            return credit_card.name
+    Usage::
+        from django.contrib import admin
+        from admin_tools.decorators import admin_link
+        from test_app.models import Foo
+
+        @admin.register(Foo)
+        class FooAdmin(admin.ModelAdmin):
+            fields = ('name', 'bar', 'bar_link')
+            readonly_fields = ('bar_link',)
+
+            @admin_link('bar')
+            def bar_link(self, obj):
+                return obj.name
+
+    .. image:: images/decorators/decorator_admin_link.png
     """
 
     def wrap(func):
@@ -44,22 +59,38 @@ def admin_link(attr, short_description=None, empty_description="-"):
 
 def admin_changelist_link(attr, short_description=None, empty_description="-", query_string=None):
     """Decorator used for rendering a link to the list display of
-    a related model in the admin detail page.
-    attr (str):
+    a related model in the admin changelist page.
+
+    ``attr (str)``
         Name of the related field.
-    short_description (str):
+    ``short_description (str)``
         Field display name.
-    empty_description (str):
+        Default value: None.
+    ``empty_description (str)``
         Value to display if the related field is None.
-    query_string (function):
+        Default value: -.
+    ``query_string (function)``
         Optional callback for adding a query string to the link.
         Receives the object and should return a query string.
+        Default value: None.
+
     The wrapped method receives the related object and
     should return the link text.
-    Usage:
-        @admin_changelist_link('products', _('Products'), query_string=lambda c: 'category_id={}'.format(c.pk))
-        def credit_card_link(self, credit_card):
-            return credit_card.name
+    Usage::
+        from admin_tools.decorators import admin_changelist_link
+        from django.contrib import admin
+        from test_app.models import Bar
+
+        @admin.register(Bar)
+        class BarAdmin(admin.ModelAdmin):
+            fields = ('name', 'foos', 'custom_field')
+            readonly_fields = ('foos', 'custom_field')
+
+            @admin_changelist_link('foos', query_string=lambda bar: f'bar_id={bar.pk}')
+            def foos(self, foos):
+                return ', '.join(str(foo) for foo in foos.all())
+
+    .. image:: images/decorators/decorator_admin_changelist_link.png
     """
 
     def wrap(func):
@@ -80,21 +111,32 @@ def admin_changelist_link(attr, short_description=None, empty_description="-", q
     return wrap
 
 
-def admin_field(short_description=None, empty_description='-', allow_tags=True):
-    """Decorator used for rendering a link to a related model in
+def admin_field(short_description=None, allow_tags=True):
+    """Decorator used for custom field or add link in
     the admin detail page.
-    attr (str):
-        Name of the related field.
-    short_description (str):
-        Name if the field.
-    allow_tags (bool):
+
+    ``short_description (str)``
+        Description of the field.
+    ``allow_tags (bool)``
         Allow tags.
-    The wrapped method receives the related object and should
-    return the link text.
-    Usage:
-        @admin_link('change password', _('Change password'))
-        def change_password(self, obj):
-            return format_html(f'<a href="{obj.id}/password/">change password</a>')
+
+    The wrapped method receives the custom field.
+    Usage::
+        from admin_tools.decorators import admin_field
+        from django.contrib import admin
+        from django.utils.safestring import mark_safe
+        from test_app.models import Bar
+
+        @admin.register(Bar)
+        class BarAdmin(admin.ModelAdmin):
+            fields = ('name', 'foos', 'custom_field')
+            readonly_fields = ('foos', 'custom_field')
+
+            @admin_field('Custom field')
+            def custom_field(self, obj):
+                return mark_safe('<h1>custom field</h1>')
+
+    .. image:: images/decorators/decorator_admin_field.png
     """
 
     def wrap(func):
