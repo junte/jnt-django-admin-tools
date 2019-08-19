345ed6e6fdd139d44e7c1b1ef8672747999743b9
Andrey Surovov <andrey.surovov@gmail.com>
2019-08-19 16:48:49 +0300

    [#21] Refactor, separate code by classes

--- src/admin_tools/mixins.py
+++ src/admin_tools/mixins.py
@@ -65,7 +65,7 @@ class AdminAutocompleteFieldsMixin:
         ]))
 
 
-class GenericForeignKeyMixin:
+class BaseGenericForeignKeyMixin:
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj=obj)
 
@@ -107,18 +107,24 @@ class GenericForeignKeyMixin:
             fields.append(value)
         return fields
 
+    def _set_validators_gfk(self, form):
+        for field in self._get_generic_foreign_keys(self.model):
+            _copy_func = copy_func(self._clean_formfield)
+            _copy_func.__defaults__ = (field,)
 
+            setattr(form, f'clean_{field.name}', _copy_func)
 
+    def _clean_formfield(self, gfk_field):
+        ct_field = self.cleaned_data.get(gfk_field.ct_field)
+        fk_field = self.cleaned_data.get(gfk_field.fk_field)
 
-class GenericForeignKeyInlineAdminMixin(GenericForeignKeyMixin):
-    def get_formset(self, request, obj, **kwargs):
-        self._set_declared_gfk()
-        formset = super().get_formset(request, obj, **kwargs)
-        self._set_validators_gfk(formset.form)
+        if ct_field and fk_field:
+            return
+        elif not ct_field and not fk_field:
+            return
 
-        # set clean for gfk
-        return formset
-
+        msg = _('Both values must be filled or cleared')
+        self.add_error(gfk_field.ct_field, msg)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         generic_foreign_keys = self._get_generic_foreign_keys(self.model)
@@ -145,6 +151,22 @@ class GenericForeignKeyInlineAdminMixin(GenericForeignKeyMixin):
 
         return form_field
 
+    def _get_gfk_ids_related_models(self, gfk):
+        related_models = gfk.get_related_models()
+
+        content_types = ContentType.objects.get_for_models(*related_models)
+
+        return [x.id for x in content_types.values()]
+
+    def _set_declared_gfk(self):
+        for field in self._get_generic_foreign_keys(self.model):
+            form_char_field = forms.CharField(
+                widget=forms.HiddenInput(),
+                required=False,
+            )
+
+            self.form.declared_fields[field.name] = form_char_field
+
     def _is_generic_foreign_key(self, field_name, generic_foreign_keys):
         for gfk in generic_foreign_keys:
             if field_name in (gfk.name, gfk.ct_field, gfk.fk_field):
@@ -162,44 +184,17 @@ class GenericForeignKeyInlineAdminMixin(GenericForeignKeyMixin):
             )
         }
 
-    def _get_gfk_ids_related_models(self, gfk):
-        related_models = gfk.get_related_models()
 
-        content_types = ContentType.objects.get_for_models(*related_models)
+class GenericForeignKeyInlineAdminMixin(BaseGenericForeignKeyMixin):
+    def get_formset(self, request, obj, **kwargs):
+        self._set_declared_gfk()
+        formset = super().get_formset(request, obj, **kwargs)
+        self._set_validators_gfk(formset.form)
 
-        return [x.id for x in content_types.values()]
-
-    def _set_validators_gfk(self, form):
-        for field in self._get_generic_foreign_keys(self.model):
-            _copy_func = copy_func(self._clean_formfield)
-            _copy_func.__defaults__ = (field,)
-
-            setattr(form, f'clean_{field.name}', _copy_func)
-
-    def _clean_formfield(self, gfk_field):
-        ct_field = self.cleaned_data.get(gfk_field.ct_field)
-        fk_field = self.cleaned_data.get(gfk_field.fk_field)
-
-        if ct_field and fk_field:
-            return
-        elif not ct_field and not fk_field:
-            return
-
-        msg = _('Both values must be filled or cleared')
-        self.add_error(gfk_field.ct_field, msg)
-
-    def _set_declared_gfk(self):
-        for field in self._get_generic_foreign_keys(self.model):
-            form_char_field = forms.CharField(
-                widget=forms.HiddenInput(),
-                required=False,
-            )
-
-            self.form.declared_fields[field.name] = form_char_field
+        return formset
 
 
-
-class GenericForeignKeyAdminMixin(GenericForeignKeyMixin):
+class GenericForeignKeyAdminMixin(BaseGenericForeignKeyMixin):
     def get_fieldsets(self, request, obj=None):
         fieldsets = super().get_fieldsets(request, obj=obj)
 
@@ -216,66 +211,19 @@ class GenericForeignKeyAdminMixin(GenericForeignKeyMixin):
 
         return form
 
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        generic_foreign_keys = self._get_generic_foreign_keys(self.model)
-
-        is_gfk, gfk = self._is_generic_foreign_key(
-            db_field.name, generic_foreign_keys
-        )
-
-        form_field = super().formfield_for_foreignkey(
-            db_field, request, **kwargs
-        )
-
-        if not is_gfk:
-            return form_field
-
-        attrs = self._get_content_type_autocomplete_attrs(gfk)
-        db = kwargs.get('using')
-        form_field.widget = ContentTypeAutocompleteSelect(
-            db_field.remote_field,
-            self.admin_site,
-            using=db,
-            attrs=attrs
-        )
-
-        return form_field
-
-    def _is_generic_foreign_key(self, field_name, generic_foreign_keys):
-        for gfk in generic_foreign_keys:
-            if field_name in (gfk.name, gfk.ct_field, gfk.fk_field):
-                return True, gfk
-        return False, None
-
-    def _get_gfk_ids_related_models(self, gfk):
-        related_models = gfk.get_related_models()
-
-        content_types = ContentType.objects.get_for_models(*related_models)
-
-        return [x.id for x in content_types.values()]
-
-    def _set_declared_gfk(self):
-        for field in self._get_generic_foreign_keys(self.model):
-            form_char_field = forms.CharField(
-                widget=forms.HiddenInput(),
-                required=False,
-            )
-
-            self.form.declared_fields[field.name] = form_char_field
-
     def _set_present_gfk(self, form, obj):
         for field in self._get_generic_foreign_keys(self.model):
             if field.fk_field in form.base_fields:
                 form.base_fields[field.fk_field].widget.attrs[
                     'data-present'] = str(getattr(obj, field.name, ''))
 
-    def get_inline_formsets(self, request, formsets, inline_instances, obj=None):
+    def get_inline_formsets(self, request, formsets, inline_instances,
+                            obj=None):
         self.set_present_for_inlines(request, formsets, inline_instances, obj)
 
         return super().get_inline_formsets(
             request, formsets, inline_instances, obj=obj
         )
-
 
     def set_present_for_inlines(self, request, formsets, inline_instances, obj):
         for formset, inline in zip(formsets, inline_instances):
@@ -291,37 +239,9 @@ class GenericForeignKeyAdminMixin(GenericForeignKeyMixin):
                     if gfk.ct_field not in inline_fields:
                         continue
 
-                    form.fields[gfk.fk_field].widget.attrs['data-present'] = str(getattr(form.instance, gfk.name, ''))
-
-    def _get_content_type_autocomplete_attrs(self, gfk):
-        return {
-            'class': 'generic-foreign-key-field',
-            'data-ct--field': gfk.ct_field,
-            'data-fk--field': gfk.fk_field,
-            'data-gf--name': gfk.name,
-            'data-gfk--models': ','.join(
-                str(x) for x in self._get_gfk_ids_related_models(gfk)
-            )
-        }
-
-    def _set_validators_gfk(self, form):
-        for field in self._get_generic_foreign_keys(self.model):
-            _copy_func = copy_func(self._clean_formfield)
-            _copy_func.__defaults__ = (field,)
-
-            setattr(form, f'clean_{field.name}', _copy_func)
-
-    def _clean_formfield(self, gfk_field):
-        ct_field = self.cleaned_data.get(gfk_field.ct_field)
-        fk_field = self.cleaned_data.get(gfk_field.fk_field)
-
-        if ct_field and fk_field:
-            return
-        elif not ct_field and not fk_field:
-            return
-
-        msg = _('Both values must be filled or cleared')
-        self.add_error(gfk_field.ct_field, msg)
+                    form.fields[gfk.fk_field].widget.attrs[
+                        'data-present'] = str(
+                        getattr(form.instance, gfk.name, ''))
 
     def _update_gfk_fieldsets(self, fieldsets):
         gfk_fields = self._get_generic_foreign_keys(self.model)
