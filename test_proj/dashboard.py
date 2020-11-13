0f575fbedf4e9651d98ee3f98ca0634847eedac9
Stanislav Lapshin <sm.lapshin@gmail.com>
2020-11-13 11:26:28 +0300

    Change ugettext_lazy -> gettext_lazy, update version 0.9.13

--- test_proj/dashboard.py
+++ test_proj/dashboard.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from jnt_admin_tools.dashboard import Dashboard, AppIndexDashboard
 from jnt_admin_tools.dashboard import modules
