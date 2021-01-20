5c15d57f76e34f50253cd9370571bbb010358cf1
Andrey Surovov <andrey.surovov@gmail.com>
2021-01-20 17:05:07 +0300

    Refactor file names #48

--- src/jnt_admin_tools/mixins/__init__.py
+++ src/jnt_admin_tools/mixins/__init__.py
@@ -1,7 +1,7 @@
-from .autocomplete_fields_admin import AutocompleteFieldsAdminMixin
+from .autocomplete_fields import AutocompleteFieldsAdminMixin
 from .generic_foreign_key import GenericForeignKeyAdminMixin
 from .generic_foreign_key_inline import GenericForeignKeyInlineAdminMixin
-from .clickable_links_admin import ClickableLinksAdminMixin
-from .autocomplete_changelist_filters_admin import (
+from .clickable_links import ClickableLinksAdminMixin
+from .autocomplete_changelist_filters import (
     AutocompleteChangelistFiltersAdminMixin,
 )
