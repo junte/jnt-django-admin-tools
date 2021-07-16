b059cb0cbcfe05aeda954cab08a01f6e4a757404
Andrey Surovov <andrey.surovov@gmail.com>
2021-07-16 16:12:08 +0300

    Fix AdminChangeListField: 0.9.41 -> 0.9.42 #60

--- src/jnt_admin_tools/admin/fields/changelist.py
+++ src/jnt_admin_tools/admin/fields/changelist.py
@@ -17,24 +17,13 @@ class AdminChangeListField:
         self._model_admin = model_admin
         self._field_name = field_name
         self._readonly_widget = readonly_widget
-        self.__name__ = self._get_label()
+        self.__name__ = field_name
 
     def __call__(self, instance) -> str:
         """Implement callable for render widget."""
+        model_field = self._model_admin.model._meta.get_field(self._field_name)
         return self._readonly_widget.render(
             getattr(instance, self._field_name),
             self._field_name,
-            db_field=self._get_model_field(),
+            db_field=model_field,
         )
-
-    def _get_model_field(self) -> models.Field:
-        """Get current model field."""
-        return self._model_admin.model._meta.get_field(self._field_name)
-
-    def _get_label(self) -> str:
-        """Get label for column."""
-        model_field = self._get_model_field()
-        try:
-            return str(model_field.verbose_name)
-        except AttributeError:
-            return self._field_name
