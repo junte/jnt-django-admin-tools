96c5720db9d934f10667dc5df8f56b2272fd6329
Andrey Surovov <andrey.surovov@gmail.com>
2021-07-19 16:42:25 +0300

    Fix permissions readonly widget: 0.9.42 -> 0.9.43 #61

--- src/jnt_admin_tools/widgets/readonly/permissions_readonly.py
+++ src/jnt_admin_tools/widgets/readonly/permissions_readonly.py
@@ -1,21 +1,29 @@
 from django.forms.models import ModelChoiceIterator
+
 from jnt_admin_tools.widgets import PermissionSelectMultipleWidget
+
+
+class FakeField:
+    """Fake field for ModelChoiceIterator."""
+
+    def __init__(self, queryset) -> None:
+        self.queryset = queryset
+        self.empty_label = False
 
 
 class PermissionSelectMultipleReadonlyWidget(PermissionSelectMultipleWidget):
     """Permissions multiple readonly widget. """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         """Initializing."""
-        self._formfield = kwargs.pop("formfield", None)
         super().__init__(*args, **kwargs)
-
         self.extra_data["is_readonly"] = True
 
-        if self._formfield:
-            self.choices = ModelChoiceIterator(self._formfield)
-
-    def render(self, name, value, *args, **kwargs):
+    def render(self, value, name, *args, **kwargs):
         """Render html-string."""
-        self.groups_permissions = value.all()
-        return super().render(name=name, value=value.all(), *args, **kwargs)
+        permissions = value.all()
+
+        self.choices = ModelChoiceIterator(FakeField(permissions))
+        self.groups_permissions = permissions
+
+        return super().render(name=name, value=permissions)
