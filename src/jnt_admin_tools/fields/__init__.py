67c31862882bad70362f70bfcf35b6a9199c6047
Andrey Surovov <andrey.surovov@gmail.com>
2021-12-03 15:23:49 +0000

    refactor!: remove old classes #65

--- src/jnt_admin_tools/fields/__init__.py
+++
@@ -1,1 +0,0 @@
-from .permissions import PermissionSelectMultipleField
--- src/jnt_admin_tools/widgets/readonly/base.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-
-class BaseReadOnlyWidget(abc.ABC):
-    """Base readonly widget."""
-
-    @abc.abstractmethod
-    def render(self, field_value, field_name, **kwargs) -> str:
-        """Render current widget."""
